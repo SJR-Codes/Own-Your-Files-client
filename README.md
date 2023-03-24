a3b42350b4c16caaa26d5bc0bca64f378ada1639
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-03-24 13:07:54 +0200

    added fetch photos by category

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Own-Your-Files-client
-Client for Own Your Files – fotos REST API
+## SJR-Codes.github.io
+## Demo client for:
+# Own Your Files – Photos
+https://github.com/SJR-Codes/OwnYourFiles_foto
+
+### Description:
+
+Simple & very lightweight web-client for Own Your Files – Photos HTTP REST API
+
+#### Source:
+
+https://github.com/SJR-Codes/Own-Your-Files-client/tree/dev
+
+### Testing:
+
+https://sjr-codes.github.io/
+
+Username: test@example.com
+
+Password: Testi42
+
+## Feedback welcome!
+
+(It ain't pretty but she's mine, besides I'm more of a backend guy...)
+
+
+## Updates:
+2023-03-24: added fetch photos by category
--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = "http://localhost:8000";
 let cont = document.getElementById('content');
 let foot = document.getElementById("footer");
 let userEmail = false;
-let isAdmin = false; //relax, it's just for showing or not certain forms, your beh... backend is safe
+let isAdmin = false; //relax, it's just for showing or not certain forms, your beh... backend is safe (if it's any good)
 let categories = false;
 //TODO: we're getting lot of globals lately, is this the way?
 
-function getInit() {
+function getInit(method="GET", accept="application/json", contentType="application/json", withToken=true, data=false ) {
     let myInit = {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization: "Bearer " + sessionStorage.getItem('token'),
-        },
+        method: method,
         mode: "cors",
         cache: "default",
+        headers: {
+            'Accept': accept,
+        },
     };
+
+    if ( contentType !== false ) {
+        myInit.headers['Content-Type'] = contentType;
+    }
+    if ( withToken !== false ) {
+        myInit.headers['Authorization'] = "Bearer " + sessionStorage.getItem('token');
+    }
+    if ( data !== false ) {
+        myInit['body'] = data;
+    }
+
     return myInit;
 }
 async function goFetch(request){
     const response = await fetch(request);
+    //console.debug("Res: " + response.json())
     if (response.status === 200) {
         return await response.json();
     }
@@ -29,26 +40,32 @@ async function goFetch(request){
         return false;
     }
 }
-async function getPhotos() {
+async function getPhotos(category_id = false) {
     let myInit = getInit();
-    const request = new Request(baseURL + "/photos/", myInit);
-    let res = await goFetch(request);
-    if ( res !== false ) {
-        cont.innerHTML = "";
-        res.forEach((photo) => {
-            let click = 'onclick="getPhoto(\'' + photo.id + '\')"';
-            let img = "<img src='data:image/jpeg;base64," + photo.thumbnail + "' alt='photo'></img>";
-            let span = "<span " + click + " class='thumb'>" + img + "<span/>";
-            cont.innerHTML += span;  
-        });
+    let req = ''
+    if ( category_id != false ) {
+        req = "/photosbycat/"  + category_id;
     }
+    else {
+        req = "/photos/";
+    }
+    const request = new Request(baseURL + req, myInit);
+    let res = await goFetch(request);
+    //if ( res !== false ) {        //TODO: better error checking
+    res.forEach((photo) => {
+        let click = 'onclick="getPhoto(\'' + photo.id + '\')"';
+        let img = "<img src='data:image/jpeg;base64," + photo.thumbnail + "' alt='photo'></img>";
+        let span = "<span " + click + " class='thumb'>" + img + "<span/>";
+        cont.innerHTML += span;  
+    });
+    //}
 }
 async function getPhoto(id) {
     const fpath = "/photos/" + id;
     let myInit = getInit();
     const request = new Request(baseURL + fpath, myInit);
     let res = await goFetch(request);
-    //console.debug(res)
+    //console.debug("Res: " + res)
     if ( res !== false ) {
         let click = 'onclick="getFullPhoto(\'' + id + '\')"';
         let img = "<img src='data:image/jpeg;base64," + res.image + "' alt='photo'></img>";
@@ -62,29 +79,32 @@ async function getPhoto(id) {
 }
 async function doLogin(e) {
     e.preventDefault();
+    console.debug("Trying login...")
     const usern = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const password = document.getElementById('password').value;    
     if( usern == "" || password == "" ) {
         cont.innerHTML = "<div class='error'>Enter username and password, please.</div>" + cont.innerHTML;
         return null;
     }
+
+    //hide form after getting both values
+    document.getElementById('login-form').classList.add("hidden");
+    //TODO: some nice rolling hourglass would be nice
+    cont.innerHTML = "<br><br><h3>One moment, please. Loggin you in...</h3>" + cont.innerHTML;
+
+    //make request
     const queryParams = { username: usern, password: password }
-    const formbody = new URLSearchParams(queryParams).toString();    
-    const request = new Request(baseURL + "/auth/jwt/login", {
-        method: "POST",
-        mode: "cors", // no-cors, *cors, same-origin
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: formbody,
-    });
+    const formbody = new URLSearchParams(queryParams).toString();
+    let myInit = getInit("POST", "application/json", "application/x-www-form-urlencoded", false, formbody );
+    const request = new Request(baseURL + "/auth/jwt/login", myInit);
     let res = await goFetch(request);
+    
     if ( res !== false ) {
         sessionStorage.setItem('token', res.access_token);
         await setUserInfo();
         await getCategories();
         await showNavi();
-        await getPhotos();
+        await showPhotos();
     }
 }
 async function AddCategory(e) {
@@ -95,17 +115,10 @@ async function AddCategory(e) {
         return null;
     }    
     let data = JSON.stringify({"title": cat_title});
-    const request = new Request(baseURL + "/categories/", {
-        method: "POST",
-        mode: "cors", // no-cors, *cors, same-origin
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': "Bearer " + sessionStorage.getItem('token'),
-            'Content-Type': 'application/json',
-        },
-        body: data,
-    });
+    let myInit = getInit("POST", "application/json", "application/json", true, data );
+    const request = new Request(baseURL + "/categories/", myInit);
     let res = await goFetch(request);
+    
     if ( res !== false ) {
         await getCategories();
         await showSettings();
@@ -127,7 +140,7 @@ async function showDivider() {
 }
 //TODO: daamn, tis' ugly as fudge
 async function showNavi() {
-    let navi = `<p id="footnavi"><span class="fakelink" onclick="getPhotos()">Photos</span> | `;
+    let navi = `<p id="footnavi"><span class="fakelink" onclick="showPhotos()">Photos</span> | `;
     if ( isAdmin == true ) { navi += `<span class="fakelink" onclick="showUpload()">Upload</span> | `; }
     navi += `<span class="fakelink" onclick="showSettings()">Settings</span></p>`;
 
@@ -144,6 +157,14 @@ async function setUserInfo() {
         isAdmin = res.is_superuser;
     }
 }
+async function showPhotos(category_id) {
+    let list = await categoriesSelect(category_id, true);    
+    let form = '<form action="" id="search-form">';
+    
+    cont.innerHTML = form + list + '</form>';
+
+    await getPhotos(category_id);
+}
 async function showUserInfo(clear = false) {
     if ( userEmail !== false ) {
         //cont.innerHTML = JSON.stringify(res);
@@ -151,7 +172,7 @@ async function showUserInfo(clear = false) {
             `<h3>Your email:</h3>
             <form action="" id="user-form">
             <input type="text" name="email" id="email" class="text-input" value="${userEmail}"><br><br>
-            <button class="button" onclick="UpdateUser(event)">Change</button></form>`;
+            <!--<button class="button" onclick="UpdateUser(event)">Change</button>--></form>`;
         clear == true ? cont.innerHTML = uinfo : cont.innerHTML += uinfo;
     };
 }
@@ -186,19 +207,33 @@ function showUpload() {
     let form = `<h3>Upload new photo:</h3><form action="" id="up-form">
     <input type="file" name="upfile" id="upfile" class="text-input" placeholder=""><br><br>`;
 
-    if ( categories !== false ) {
-        list = "<select class='text-input' id='category_id' name='category_id'>";
-        list += "<option value=''> - select category - </option>";
-        categories.forEach((category) => {
-            list += "<option value='" + category.id + "'>" + category.title + "</option>";
-        });
-        list += "</select>"
-    }
+    let list = categoriesSelect();
 
     form += list + `<br><br><button class="button" onclick="doUpload(event)">Send</button></form>`;
 
     cont.innerHTML = form;
 }
+function categoriesSelect( category_id = false, onchange = false ) {
+    if ( categories !== false ) {
+        let change = onchange == true ? ' onchange="doSearch()"' : ''
+        list = "<select class='text-input' id='category_id' name='category_id'" + change + ">";
+        list += "<option value=''> - select category - </option>";
+        categories.forEach((category) => {
+            let optSelected = category.id == category_id ? " selected='selected'" : "";
+            list += "<option value='" + category.id + "'" + optSelected + ">" + category.title + "</option>";
+        });
+        list += "</select>"
+    }
+
+    return list;
+}
+
+async function doSearch() {
+    const category_id = document.getElementById("category_id").value;
+    cont.innerHTML = "<h3>Searching... One moment, please.</h3>";
+    await showPhotos(category_id);
+}
+
 async function doUpload(e) {
     e.preventDefault();
     const upfile = document.getElementById("upfile");
@@ -214,24 +249,11 @@ async function doUpload(e) {
     form = new FormData(document.getElementById('up-form'));
 
     //TODO: error checking needed here...
-    let res = await upStuff("/upload/", form);
+    let myInit = getInit("POST", "application/json", false, true, form );
+    const request = new Request(baseURL + "/upload/", myInit);
+    let res = await goFetch(request);
+
     console.debug(res);
     await getPhoto(res.id);
-    cont.innerHTML += '<div class="success">Photo uploaded very sucessfully!</div>';
-}
-async function upStuff(fpath, body){
-    let request = new Request(baseURL + fpath, {
-        method: "POST",
-        mode: "cors", // no-cors, *cors, same-origin
-        headers: {
-            'Accept': 'application/json',
-            //'Content-Type': 'multipart/form-data', //i'll remember you, you puny piece of sales
-            'Authorization': "Bearer " + sessionStorage.getItem('token'),
-        },
-        body: body, 
-    });
-    let res = await goFetch(request);
-    if ( res !== false ) {
-        return res;
-    }
+    cont.innerHTML = '<div class="success">Photo uploaded very sucessfully!</div>' + cont.innerHTML;
 }
